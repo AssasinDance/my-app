55bfdbca6fd4cda76ac492a0d0fe35574d035b8d
Danil Buntovskikh <buntovskikhds@gmail.com>
2025-03-23 13:47:25 +0300

    all files loaded

---
+++ src/components/footer/footer.js
@@ -0,0 +1,2 @@
+import './footer.css';
+import './task-filter/task-filter.js';
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './footer/footer.js';
+import './new-task-form/new-task-form.js';
+import './tsk-list/task-list.js';
